e5682d7e59ae4a6c8340943205b0174a042241b8
wangxf123456 <wangxf@umich.edu>
2015-07-09 13:03:50 +0800

    Initial Commit

---
+++ PdfSearcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PdfSearcher
+//
+//  Created by WangXiaofei on 15/7/9.
+//  Copyright (c) 2015年 WangXiaofei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PdfSearcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PdfSearcher
+//
+//  Created by WangXiaofei on 15/7/9.
+//  Copyright (c) 2015年 WangXiaofei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
